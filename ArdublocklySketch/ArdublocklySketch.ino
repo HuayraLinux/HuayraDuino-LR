cd3cad98b619cc16f7f2a606a3cd592c389102a2
Daniel Ferro <danieloscarferro@gmail.com>
2023-03-27 11:39:22 -0300

    DS18B20 final

--- ArdublocklySketch/ArdublocklySketch.ino
+++ ArdublocklySketch/ArdublocklySketch.ino
@@ -5,8 +5,8 @@ float i;
 float k;
 String m;
 String n;
-uint8_t direccion_3[8];
 uint8_t direccion_4[8];
+uint8_t direccion_5[8];
 
 DS18B20 ds_1(2);
 DS18B20 ds_2(3);
@@ -14,15 +14,14 @@ DS18B20 ds_2(3);
 void setup() {
   Serial.begin(9600);
   ds_1.selectNext();
-  ds_1.getAddress(direccion_3);
+  ds_1.getAddress(direccion_4);
   ds_2.selectNext();
-  ds_2.getAddress(direccion_4);
+  ds_2.getAddress(direccion_5);
 }
 
 void loop() {
   i = ds_1.getTempC();
   k = ds_2.getTempC();
-
   m = String("DS_1,") + String(i);
   n = String(",DS_2,") + String(k);
   Serial.println((String(m) + String(n)));
