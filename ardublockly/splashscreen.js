f8c992f7c5a674b212f1fec6edc285daed174cfd
Daniel Ferro <danieloscarferro@gmail.com>
2023-08-10 16:49:03 -0300

    version en splash

--- ardublockly/splashscreen.js
+++ ardublockly/splashscreen.js
@@ -3,8 +3,8 @@ import data from '../package.json' assert { type: 'json' };
 var splashScreen = document.querySelector('.splash');
 var version_hd = document.getElementById('version_hd');
 
-  splashScreen.style.opacity = 0;
   version_hd.innerHTML = "versión " + data['version'];
+  splashScreen.style.opacity = 0;
   setTimeout(()=>{
     splashScreen.classList.add('hidden')
   },3100)
